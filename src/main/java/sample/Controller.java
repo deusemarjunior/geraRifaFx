94bcfee6fddb499b2392518abe35e69404274d9a
Deusemar Bezerra Chaves Junior <djrweb@gmail.com>
2017-01-26 01:01:37 -0300

    Adicionando a possibilidade de selecionar Quantidades de dezenas 3, 2, 1

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -5,42 +5,75 @@ import java.util.List;
 
 public class Controller {
 
-    List<ColunasNumeros> listColunaNumeros = new ArrayList<ColunasNumeros>();
+    List<String> numeros = new ArrayList<String>();
 
     Integer controle = 1;
-    Integer controleInterno= 1;
 
     /*****
+     * Para 3 dezenas
+     *
      * 1 ate 333
      *
      * 334 ate 667
      *
      * 667 ate 999
      *
-     * @return
+     * ====
+     *
+     * Para 2 Dezenas
+     *
+     * 1 até 499
+     *
+     * 500 até 999
+     *
+     * ===
+     *
+     * Para 1 Dezena 999
+     *
+     * @return List<Stirng></Stirng>
      */
+    public List<String> gerarSequencias(Integer qtdDezenas){
 
-    public List<ColunasNumeros> gerarSequencias(){
+        if(qtdDezenas == 3 ) {
+            Integer col1 = 1, col2 = 334, col3 = 667;
 
-        Integer col1 = 1 , col2 = 334, col3 = 667;
+            numeros.add(String.format("  %s     %s      %s ", col1, col2, col3));
+            for (controle = 1; controle < 333; controle++) {
+                numeros.add(String.format("  %s     %s      %s ",col1 + controle, col2 + controle, col3 + controle));
+            }
+        }
+        if(qtdDezenas == 2){
+            Integer col1 = 1, col2 = 500;
 
-        listColunaNumeros.add(new ColunasNumeros(col1,col2,col3));
-        for (controle = 1; controle <333 ; controle ++){
-            listColunaNumeros.add(new ColunasNumeros(col1+controle,col2+controle,col3+controle));
+            numeros.add(String.format("   %s       %s  ", col1, col2));
+            for (controle = 1; controle < 499; controle++) {
+                if(controle == 498)
+                    numeros.add(String.format("  %s     %s      %s ",col1 + controle, col2 + controle, 999));
+                else
+                    numeros.add(String.format("   %s       %s  ",col1 + controle, col2 + controle));
+
+            }
+
+        }
+        if(qtdDezenas == 1){
+            Integer col1 = 1;
+
+            numeros.add(String.format("     %s      ", col1));
+            for (controle = 1; controle < 999; controle++) {
+                numeros.add(String.format("     %s      ",col1 + controle));
+            }
         }
 
-
-        for (ColunasNumeros listColunaNumero : listColunaNumeros) {
-            System.out.println("Lista --> " +listColunaNumero.getColuna1() +
-                                      " - " +listColunaNumero.getColuna2() +
-                                      " - " +listColunaNumero.getColuna3());
+        for (String linha : numeros) {
+       //     System.out.println(" Linha --> " + linha );
         }
-        return listColunaNumeros;
+
+        return numeros;
     }
 
     public static void main(String[] args) {
         Controller controller  = new Controller();
-        controller.gerarSequencias();
+        controller.gerarSequencias(2);
     }
 
 
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,36 +1,167 @@
 package sample;
 
-import com.javafx.tools.doclets.internal.toolkit.util.DocFinder;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
-import java.util.List;
+import javafx.scene.text.Text;
+import javafx.event.ActionEvent;
+import javafx.stage.StageStyle;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import sun.util.resources.LocaleData;
 
-import java.util.ArrayList;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class Main extends Application {
 
     Controller controller = new Controller();
 
-    List<ColunasNumeros> colunasNumerosList = new ArrayList<ColunasNumeros>();
+    private Stage primaryStage;
 
+
+    @FXML
+    Label mensagemLabel = new Label();
+
+    @FXML
     Label tituloLabel = new Label("Título");
+    @FXML
     Label descricaoLabel = new Label("Descrição");
+    @FXML
     Label valorLabel = new Label("Valor");
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Gerador de Rifa Loteria Federal");
-        primaryStage.setScene(new Scene(root, 500, 500));
-        primaryStage.show();
-    }
+    @FXML
+    TextField tituloField = new TextField();
+    @FXML
+    TextField descricaoField = new TextField();
+    @FXML
+    TextField valorField = new TextField(){
+        @Override public void replaceText(int start, int end, String text) {
+            // If the replaced text would end up being invalid, then simply
+            // ignore this call!
+            if (!text.matches("[a-z]")) {
+                super.replaceText(start, end, text);
+            }
+        }
 
+        @Override public void replaceSelection(String text) {
+            if (!text.matches("[a-z]")) {
+                super.replaceSelection(text);
+            }
+        }
+    };
+
+    @FXML
+    DatePicker dataRifa = new DatePicker();
+
+    @FXML
+    Button btnGerarRifa = new Button();
+
+    @FXML
+    ChoiceBox qtdDezenas = new ChoiceBox();
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("index.fxml"));
+        primaryStage.setTitle("Gerador de Rifa Loteria Federal - By Deusemar Junior f: (91) 991303340");
+        primaryStage.setScene(new Scene(root, 600, 300));
+        primaryStage.show();
+        mensagemLabel.setText("");
+
+    }
 
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
+
         launch(args);
     }
+
+    public void onGerarRifa(ActionEvent event){
+        System.out.println("Iniciou a Geração da Rifa");
+
+        if(tituloField.getText() != null && !tituloField.getText().isEmpty()
+                && descricaoField.getText() != null && !descricaoField.getText().isEmpty()
+                && valorField.getText() != null && !valorField.getText().isEmpty()
+                && qtdDezenas.getValue() != null
+                && dataRifa.getValue() != null){
+            mensagemLabel.setText("");
+
+
+            Stage dialog = new Stage();
+            dialog.setResizable(true);
+            dialog.initModality(Modality.APPLICATION_MODAL);
+            dialog.initOwner(primaryStage);
+            dialog.initStyle(StageStyle.UTILITY);
+
+            HashMap<String,Object> parametros = new HashMap<String,Object>();
+
+            System.out.println("Quantidade de dez " + qtdDezenas.getValue());
+
+            parametros.put("TITULO",tituloField.getText());
+            parametros.put("DESCRICAO",descricaoField.getText());
+            parametros.put("VALOR",valorField.getText());
+            //LocalDate _dataRifa = dataRifa.getValue();
+
+            parametros.put("DATARIFA", DateUtils.asDate(dataRifa.getValue()));
+
+            JasperPrint jasperPrint=null;
+
+            List<String> listNumeros = controller.gerarSequencias(new Integer(qtdDezenas.getValue().toString()));
+            try {
+                jasperPrint = JasperFillManager.fillReport("target/classes/rifa-10-por-pagina.jasper", parametros,
+                        new JRBeanCollectionDataSource(listNumeros));
+            } catch (JRException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            };
+
+            listNumeros = null;
+
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setInitialFileName(String.format("Rifa-%s-%s.pdf", tituloField.getText() , Calendar.getInstance().getTimeInMillis()));
+            fileChooser.setTitle("Salvar Rifa");
+
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Document", Arrays.asList("*.pdf", "*.PDF")));
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("HTML Document", Arrays.asList("*.html", "*.HTML")));
+
+            File file = fileChooser.showSaveDialog(dialog);
+            if (fileChooser.getSelectedExtensionFilter() != null && fileChooser.getSelectedExtensionFilter().getExtensions() != null) {
+                java.util.List<String> selectedExtension = fileChooser.getSelectedExtensionFilter().getExtensions();
+                try {
+                    if (selectedExtension.contains("*.pdf")) {
+                        JasperExportManager.exportReportToPdfFile(jasperPrint, file.getAbsolutePath());
+                    } else if (selectedExtension.contains("*.html")) {
+                        JasperExportManager.exportReportToHtmlFile(jasperPrint, file.getAbsolutePath());
+                    }
+                } catch (JRException e) {
+                    e.printStackTrace();
+                }}
+
+        }else{
+            mensagemLabel.setText("Preencha os campos para gerar a Rifa.");
+        }
+
+
+    }
+
 }
